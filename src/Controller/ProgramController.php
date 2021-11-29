ff03e91f7e2a2b7193fc353e5fd997f6466ec6b5
Sankatouille <Affile.benjamin@gmail.com>
2021-11-29 12:01:01 +0100

    definition des routes creation de la methode show quete 5)

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -12,10 +12,25 @@ class ProgramController extends AbstractController
      /**
      * @Route("/program/", name="program_index")
      */
+
+
+   
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
          ]);
+    }
+/**
+ *  @Route("/program/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+ *
+ * @return Response
+ */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id 
+
+    ]);
     }
 }
