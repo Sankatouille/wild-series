4b4a649582d43217513e4bb7f383ce6934f0d83d
Sankatouille <Affile.benjamin@gmail.com>
2021-11-29 10:39:04 +0100

    Creation des controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+     /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('Default/index.html.twig', [
+            'website' => 'Wild Séries',
+         ]);
+    }
+}
